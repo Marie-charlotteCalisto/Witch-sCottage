d2153d1892c6b36ccb4149606d17dfa7d2ba9017
macha <marie-charlotte.calisto@epita.fr>
2021-02-18 20:28:21 +0100

    ADD: switch cam

--- Assets/player/SelectView.cs
+++ Assets/player/SelectView.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class SelectView : MonoBehaviour
 {
+    public GameObject BuildCam;
+    public GameObject FPCam;
+    public GameObject TPCam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        BuildCam.SetActive(true);        
+        FPCam.SetActive(false);
+        //TPCam.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+            
+    }
+
+    public void onClickFP()
+    {
+        FPCam.SetActive(true);
+        BuildCam.SetActive(false);        
+        //TPCam.SetActive(false);
+ 
     }
 }
